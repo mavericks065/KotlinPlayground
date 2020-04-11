7dc3aac1c965dbaeb778e8816133b9639bc511a9
nicolas <nicolas065@gmail.com>
2020-04-11 12:28:47 +1000

    solution of exercise

--- src/main/kotlin/au/com/nig/kotlin/collections/BasicListOperations.kt
+++ src/main/kotlin/au/com/nig/kotlin/collections/BasicListOperations.kt
@@ -2,35 +2,47 @@ package au.com.nig.kotlin.collections
 
 object BasicListOperations {
     fun transformIntInStrings(myList: List<Int>): List<String> {
-        TODO()
+        return myList.map { it.toString() }
     }
 
     fun keepOnlyEvenNumbers(myList: List<Int>): List<Int> {
-        TODO()
+        return myList.filter { it % 2 == 0 }
     }
 
     fun transformInListOfChars(myList: List<String>): List<Char> {
-        TODO()
+        return myList.flatMap { str -> str.toCharArray().toList() }
     }
 
     fun keepOnlyNonNullNumbers(myList: List<Int?>): List<Int> {
-        TODO()
+        return myList.filterNotNull()
     }
 
     fun sumNumbers1(myList: List<Int>): Int {
-        TODO()
+        return myList.sum()
     }
 
     fun sumNumbers2(myList: List<Int>): Int {
-        TODO()
+        return myList.reduce { x, y -> x + y }
     }
 
     fun getNumber(myList: List<Int>, i: Int): Int {
-        TODO()
+        return myList[i]
+    }
+
+    fun getLastNumber(myList: List<Int>): Int {
+        return myList.last()
+    }
+
+    fun getFirstNumber(myList: List<Int>): Int {
+        return myList.first()
     }
 
     fun modify(myList: List<Int>): List<Int> {
-        TODO()
+        val newList = myList.toMutableList()
+        newList.add(10)
+        newList.removeAt(1)
+//        newList.remove(1)
+        return newList.toList()
     }
 
 }
