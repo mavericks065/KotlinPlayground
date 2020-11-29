2262404fae23425d625b38bad8c131bd02af74c9
Z Nick <znick@Macintosh.local>
2020-11-30 10:36:07 +1100

    Trying to test date

--- src/main/kotlin/au/com/nig/oop/basics/Employee.kt
+++ src/main/kotlin/au/com/nig/oop/basics/Employee.kt
@@ -1,10 +1,25 @@
 package au.com.nig.oop.basics
 
+import java.time.Instant
+import java.util.Calendar
+
+
+
+
 data class Employee(
     var firstName: String,
-    val lastName: String
-){
-
+    val lastName: String,
+    var dateOfBirth: Instant? = null
+) {
+    fun getAge(): Int {
+        val currentCallendar = Calendar.getInstance()
+        val birthCallendar = Calendar.getInstance()
+        currentCallendar.timeInMillis = Instant.now().toEpochMilli()
+        birthCallendar.timeInMillis = dateOfBirth!!.toEpochMilli()
+        val currentYear = currentCallendar[Calendar.YEAR]
+        val birthYear = birthCallendar[Calendar.YEAR]
+        return currentYear - birthYear
+    }
 }
 
 class Employee2 {
--- src/test/kotlin/au/com/nig/oop/basics/EmployeeTest.kt
+++ src/test/kotlin/au/com/nig/oop/basics/EmployeeTest.kt
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.time.Instant
+import java.time.LocalDate
 
 object EmployeeTest: Spek({
     describe("Employee"){
@@ -24,6 +26,22 @@ object EmployeeTest: Spek({
             println(employee.firstName)
 
             // Then
+        }
+        it("getAge() should return employee age"){
+            // Given
+            val birthDate = LocalDate.of(1965, 1,1)
+            val dateOfBirth = Instant.ofEpochMilli(birthDate.toEpochDay())
+            val employee = Employee(
+                firstName= "Test",
+                lastName= "Name",
+                dateOfBirth= dateOfBirth
+            )
+
+            // When
+            val result = employee.getAge()
+
+            // Then
+            assertEquals(55, result)
         }
     }
 })
