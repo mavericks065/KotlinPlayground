1bc2f314fe261d60ae6d2f705d63fabee0b3198b
nicolas <nicolas065@gmail.com>
2021-03-14 16:08:31 +1100

    made the first two most basic tests pass then created the 3rd one with discount. Needs to be developed

--- src/main/kotlin/au/com/nig/hattypotter/Cart.kt
+++ src/main/kotlin/au/com/nig/hattypotter/Cart.kt
@@ -3,7 +3,17 @@ package au.com.nig.hattypotter
 import java.math.BigDecimal
 
 object Cart {
-    fun getPrice(books: List<Book>): BigDecimal = TODO()
+    fun getPrice(books: List<Book>): BigDecimal {
+        return if (books.isNotEmpty()) {
+            Book.PRICE
+        } else {
+            BigDecimal.ZERO
+        }
+    }
 }
 
-data class Book(val title: String)
+data class Book(val title: String) {
+    companion object {
+        val PRICE = BigDecimal.valueOf(8)
+    }
+}
--- src/test/kotlin/au/com/nig/hattypotter/CartTest.kt
+++ src/test/kotlin/au/com/nig/hattypotter/CartTest.kt
@@ -1,8 +1,8 @@
 package au.com.nig.hattypotter
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 internal class CartTest{
     @Test
@@ -12,6 +12,26 @@ internal class CartTest{
         // When
         val result = Cart.getPrice(listOfBooks)
         // Then
-        assertEquals(0, result)
+        assertEquals(BigDecimal.ZERO, result)
+    }
+
+    @Test
+    fun `for a cart containing only ONE book the price should be 8 Euros`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume1"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(BigDecimal.valueOf(8), result)
+    }
+
+    @Test
+    fun `for a cart containing 2 different books the price should be 16 - 5% Euros`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume1"), Book("Volume2"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(BigDecimal.valueOf(15.2), result)
     }
 }
