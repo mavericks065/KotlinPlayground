aa1a642403b1bc38bb47b55dc057ae559500e05c
nicolas <nicolas065@gmail.com>
2021-03-17 17:07:14 +1100

    refactor and make tests pass

--- src/main/kotlin/au/com/nig/hattypotter/Cart.kt
+++ src/main/kotlin/au/com/nig/hattypotter/Cart.kt
@@ -4,21 +4,30 @@ import au.com.nig.hattypotter.Book.Companion.PRICE
 import au.com.nig.hattypotter.BookSetFactory.Discount.*
 import java.math.BigDecimal
 
-data class Cart(var books: List<Book> = emptyList(),
-                var priceCalculator: PriceCalculator = PriceCalculator()) {
+data class Cart(var priceCalculator: PriceCalculator = PriceCalculator(),
+                var items: List<Item> = emptyList()) {
 
     fun getCartPrice(): BigDecimal {
-        return priceCalculator.calculatePrice(books)
+        return priceCalculator.calculatePrice(items)
     }
 
-    fun addItem(newBook: Book): List<Book> {
-        books = books.plus(newBook)
-        return books;
+    fun addItem(newBook: Book): List<Item> {
+        val existingItem = items.find { it.book.equals(newBook) }
+
+        if (existingItem == null) {
+            items = items.plus(Item(newBook, 1))
+        } else {
+            existingItem.qtity = existingItem.qtity + 1
+        }
+
+        return items;
     }
 }
 
+data class Item(val book: Book, var qtity: Int)
+
 class PriceCalculator {
-    fun calculatePrice(books: List<Book>): BigDecimal {
+    fun calculatePrice(books: List<Item>): BigDecimal {
         return if (books.isNotEmpty()) {
             BookSetFactory.getOptimizedBookSetsForCart(books)
                 .map { it.books.size.toDouble().times(it.setDiscount.discount) }
@@ -32,25 +41,40 @@ class PriceCalculator {
 }
 
 object BookSetFactory {
-    enum class Discount(val nbBooks: Int, val discount: Double) {
-        NO_DISCOUNT(1, 1.0),
-        FIVE_PERCENT(2, 0.95),
-        TEN_PERCENT(3, 0.9),
-        TWENTY_PERCENT(4, 0.8),
-        TWENTY_FIVE_PERCENT(5, 0.75)
+    enum class Discount(val discount: Double) {
+        NO_DISCOUNT(1.0),
+        FIVE_PERCENT(0.95),
+        TEN_PERCENT(0.9),
+        TWENTY_PERCENT(0.8),
+        TWENTY_FIVE_PERCENT(0.75)
     }
 
-    fun getOptimizedBookSetsForCart(books: List<Book>): List<BookSet> {
-        return if (books.size == FIVE_PERCENT.nbBooks && books.map { it.title }.distinct().size == FIVE_PERCENT.nbBooks) {
-            listOf(BookSet(books.toSet(), FIVE_PERCENT))
-        } else if (books.size == TEN_PERCENT.nbBooks && books.map { it.title }.distinct().size == TEN_PERCENT.nbBooks) {
-            listOf(BookSet(books.toSet(), TEN_PERCENT))
-        } else if (books.size == TWENTY_PERCENT.nbBooks && books.map { it.title }.distinct().size == TWENTY_PERCENT.nbBooks) {
-            listOf(BookSet(books.toSet(), TWENTY_PERCENT))
-        } else if (books.size == TWENTY_FIVE_PERCENT.nbBooks && books.map { it.title }.distinct().size == TWENTY_FIVE_PERCENT.nbBooks) {
-            listOf(BookSet(books.toSet(), TWENTY_FIVE_PERCENT))
-        } else
-            listOf(BookSet(books.toSet(), NO_DISCOUNT))
+    fun getOptimizedBookSetsForCart(items: List<Item>): List<BookSet> {
+        val result: MutableList<Set<Book>> = mutableListOf(items.map { it.book }.toSet())
+
+        var areThereLeftOverItems = true
+        var leftOverItems: List<Item> = items
+        while (areThereLeftOverItems) {
+            leftOverItems = leftOverItems.map { (book, quantity) -> Item(book, quantity - 1) }
+                .filter { it.qtity > 0 }
+            if (leftOverItems.isEmpty()) {
+                areThereLeftOverItems = false
+            } else {
+                result.add(leftOverItems.map { it.book }.toSet())
+                areThereLeftOverItems = true
+            }
+
+        }
+        return result.map { books ->
+            val discount = when (books.size) {
+                2 -> FIVE_PERCENT
+                3 -> TEN_PERCENT
+                4 -> TWENTY_PERCENT
+                5 -> TWENTY_FIVE_PERCENT
+                else -> NO_DISCOUNT
+            }
+            BookSet(books, discount)
+        }.toList()
     }
 
 }
