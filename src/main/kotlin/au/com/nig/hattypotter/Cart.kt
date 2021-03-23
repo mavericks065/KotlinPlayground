f504e8ddfc4ee516d5d3679b6884f7d08b58ce6c
miranda lin <miranda.lin@online-pajak.com>
2021-03-23 15:24:21 +1100

    easy tests

--- src/main/kotlin/au/com/nig/hattypotter/Cart.kt
+++ src/main/kotlin/au/com/nig/hattypotter/Cart.kt
@@ -1,9 +1,29 @@
 package au.com.nig.hattypotter
 
+import au.com.nig.hattypotter.Book.Companion.PRICE
 import java.math.BigDecimal
 
 object Cart {
-    fun getPrice(books: List<Book>): BigDecimal = TODO()
+    val DISCOUNTS = mapOf(
+        Pair(1, BigDecimal.valueOf(1)),
+        Pair(2, BigDecimal.valueOf(0.95)),
+        Pair(3, BigDecimal.valueOf(0.90)),
+        Pair(4, BigDecimal.valueOf(0.80)),
+        Pair(5, BigDecimal.valueOf(0.75))
+    )
+
+
+    fun getPrice(books: List<Book>): BigDecimal {
+        val bookSize = books.size.toLong()
+        return PRICE
+            .times(BigDecimal.valueOf(bookSize))
+            .times(DISCOUNTS.getOrDefault(bookSize.toInt(), BigDecimal.ONE))
+    }
+
 }
 
-data class Book(val title: String)
+data class Book(val title: String) {
+    companion object {
+        val PRICE = BigDecimal.valueOf(8)
+    }
+}
--- src/test/kotlin/au/com/nig/hattypotter/CartTest.kt
+++ src/test/kotlin/au/com/nig/hattypotter/CartTest.kt
@@ -3,8 +3,9 @@ package au.com.nig.hattypotter
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
-internal class CartTest{
+internal class CartTest {
     @Test
     fun `for an empty cart the price should be 0`() {
         // Given
@@ -12,6 +13,56 @@ internal class CartTest{
         // When
         val result = Cart.getPrice(listOfBooks)
         // Then
-        assertEquals(0, result)
+        assertEquals(0, BigDecimal.ZERO.compareTo(result))
+    }
+
+    @Test
+    fun `for one book in the cart the price should be 8 EUR`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume 1"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(0, BigDecimal.valueOf(8).compareTo(result))
+    }
+
+    @Test
+    fun `for two different books in the cart the price should be 16 * 0,95 EUR`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume 1"), Book("Volume 2"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(0, BigDecimal.valueOf(15.2).compareTo(result))
+    }
+
+    @Test
+    fun `for 3 different books in the cart the price should be 24 * 0,9 EUR`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume 1"), Book("Volume 2"), Book("Volume 3"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(0, BigDecimal.valueOf(21.6).compareTo(result))
+    }
+
+    @Test
+    fun `for 4 different books in the cart the price should be 32 * 0,8 EUR`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume 1"), Book("Volume 2"), Book("Volume 3"), Book("Volume 4"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(0, BigDecimal.valueOf(25.6).compareTo(result))
+    }
+
+    @Test
+    fun `for 5 different books in the cart the price should be 40 * 0,75 EUR`() {
+        // Given
+        val listOfBooks = listOf(Book("Volume 1"), Book("Volume 2"), Book("Volume 3"), Book("Volume 4"), Book("Volume 5"))
+        // When
+        val result = Cart.getPrice(listOfBooks)
+        // Then
+        assertEquals(0, BigDecimal.valueOf(30.0).compareTo(result))
     }
 }
